4e583a1017445e5da0dd2a0aaa661acf6eec4acc
LolaEnBeta <lolitaenbeta@gmail.com>
2020-01-24 16:49:45 +0100

    Create properties for Player constructor

--- src/player.js
+++ src/player.js
@@ -1,5 +1,9 @@
 "use strict";
 
 function Player(canvas) {
-
+  this.canvas = canvas;
+  this.ctx = this.canvas.getContext("2d");
+  this.size = 80;
+  this.x = canvas / 2;
+  this.y = canvas + (this.size * 2);
 }
