5065bd15ffad12ecf76cb57c48ce08b82b7c5f96
LolaEnBeta <lolitaenbeta@gmail.com>
2020-01-27 16:20:13 +0100

    Implement draw person logic

--- src/person.js
+++ src/person.js
@@ -11,3 +11,13 @@ function Person(canvas, x) {
   this.speed = 1;
   this.life = 1;
 }
+
+Person.prototype.draw = function() {
+  this.ctx.fillStyle = "pink";
+  this.ctx.fillRect(
+    this.x,
+    this.y,
+    this.size,
+    this.size,
+  );
+}
