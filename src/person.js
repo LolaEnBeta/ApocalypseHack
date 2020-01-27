4106d1818eb0a1153367eaae6c1a6bef40b65ab3
LolaEnBeta <lolitaenbeta@gmail.com>
2020-01-27 19:10:10 +0100

    Add person image to Person object

--- src/person.js
+++ src/person.js
@@ -4,7 +4,7 @@ function Person(canvas, x) {
   this.canvas = canvas;
   this.ctx = this.canvas.getContext("2d");
 
-  this.size = 55;
+  this.size = 25;
   this.x = x;
   this.y = 0;
 
@@ -14,7 +14,10 @@ function Person(canvas, x) {
 
 Person.prototype.draw = function() {
   this.ctx.fillStyle = "pink";
-  this.ctx.fillRect(
+  var img = new Image();
+  img.src = "./images/person.png"
+  this.ctx.drawImage(
+    img,
     this.x,
     this.y,
     this.size,
