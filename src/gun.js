050a3669e095d6c17eeafbfd6eb53b3a685a7a8f
LolaEnBeta <lolitaenbeta@gmail.com>
2020-01-27 20:29:12 +0100

    Create Gun constractor

---
+++ src/gun.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function Gun(canvas, x) {
+  this.canvas = canvas;
+  this.ctx = this.canvas.getContext("2d");
+
+  this.size = 45;
+  this.x = x;
+  this.y = 0;
+
+  this.speed = 1;
+  this.shots = 5;
+}
