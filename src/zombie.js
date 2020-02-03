f5843dbf6e61926d93423277ce1538e265374810
LolaEnBeta <lolitaenbeta@gmail.com>
2020-02-03 16:44:09 +0100

    Refactor Zombie constructor to class

--- src/zombie.js
+++ src/zombie.js
@@ -1,15 +1,17 @@
 "use strict";
 
-function Zombie(canvas, x) {
-  this.canvas = canvas;
-  this.ctx = this.canvas.getContext("2d");
+class Zombie {
+  constructor (canvas, x) {
+    this.canvas = canvas;
+    this.ctx = this.canvas.getContext("2d");
 
-  this.size = 50;
-  this.x = x;
-  this.y = 0;
+    this.size = 50;
+    this.x = x;
+    this.y = 0;
 
-  this.speed = 1;
-  this.damage = 10;
+    this.speed = 1;
+    this.damage = 10;
+  }
 }
 
 Zombie.prototype.draw = function() {
