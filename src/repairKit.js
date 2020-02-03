0e82d5c5c4b191799f0d32e722799af9613b576e
LolaEnBeta <lolitaenbeta@gmail.com>
2020-02-03 16:52:36 +0100

    Refactor RepairKit class to ES6

--- src/repairKit.js
+++ src/repairKit.js
@@ -12,25 +12,25 @@ class RepairKit {
     this.speed = 1;
     this.repair = 10;
   }
-}
 
-RepairKit.prototype.draw = function() {
-  this.ctx.fillStyle = "orange";
-  var img = new Image();
-  img.src = "./images/repairKit.png";
-  this.ctx.drawImage(
-    img,
-    this.x,
-    this.y,
-    this.size,
-    this.size,
-  );
-}
+  draw() {
+    this.ctx.fillStyle = "orange";
+    const img = new Image();
+    img.src = "./images/repairKit.png";
+    this.ctx.drawImage(
+      img,
+      this.x,
+      this.y,
+      this.size,
+      this.size,
+    );
+  }
 
-RepairKit.prototype.updatePosition = function() {
-  this.y = this.y + this.speed;
-}
+  updatePosition() {
+    this.y = this.y + this.speed;
+  }
 
-RepairKit.prototype.isInsideScreen = function() {
-  return this.y + this.size + this.speed;
+  isInsideScreen() {
+    return this.y + this.size + this.speed;
+  }
 }
