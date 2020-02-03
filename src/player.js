b5c4579587fc7ca1b38c3c6a9bdbe554cf0bd23a
LolaEnBeta <lolitaenbeta@gmail.com>
2020-02-03 16:40:59 +0100

    Refactor the Player class to ES6

--- src/player.js
+++ src/player.js
@@ -17,82 +17,82 @@ class Player {
     this.life = 5;
     this.level = 1;
   }
-}
 
-Player.prototype.move = function(direction) {
-  if (direction === "left") {
-    this.x += -125;
-  } else if (direction === "right") {
-    this.x += 125;
+  move(direction) {
+    if (direction === "left") {
+      this.x += -125;
+    } else if (direction === "right") {
+      this.x += 125;
+    }
   }
-}
 
-Player.prototype.handleScreenCollision = function() {
-  var screenLeft = 200;
-  var screenRight = 575;
+  handleScreenCollision() {
+    const screenLeft = 200;
+    const screenRight = 575;
 
-  if (this.x < screenLeft) {
-    this.x = 200;
-  } else if (this.x > screenRight) {
-    this.x = 575;
+    if (this.x < screenLeft) {
+      this.x = 200;
+    } else if (this.x > screenRight) {
+      this.x = 575;
+    }
   }
-}
 
-Player.prototype.draw = function() {
-  this.ctx.fillStyle = "red";
-  var img = new Image();
-  img.src = "./images/car.png"
-  this.ctx.drawImage(
-    img,
-    this.x,
-    this.y,
-    this.width,
-    this.height,
-  );
-}
-
-Player.prototype.didCollide = function(obstacle) {
-  var playerTop = this.y;
-  var playerBottom = this.y + this.size;
-  var playerLeft = this.x;
-  var playerRight = this.x + this.size;
-
-  var obstacleTop = obstacle.y;
-  var obstacleBottom = obstacle.y + obstacle.size;
-  var obstacleLeft = obstacle.x;
-  var obstacleRight = obstacle.x + obstacle.size;
-
-  var crossLeft = obstacleLeft <= playerRight && obstacleLeft >= playerLeft;
-  var crossRight = obstacleRight >= playerLeft && obstacleRight <= playerRight;
-  var crossBottom = obstacleBottom >= playerTop && obstacleBottom <= playerBottom;
-  var crossTop = obstacleTop <= playerBottom && obstacleTop >= playerTop;
-
-  if ((crossLeft || crossRight) && (crossTop || crossBottom)) {
-    return true;
+  draw() {
+    this.ctx.fillStyle = "red";
+    const img = new Image();
+    img.src = "./images/car.png"
+    this.ctx.drawImage(
+      img,
+      this.x,
+      this.y,
+      this.width,
+      this.height,
+    );
   }
-  return false;
-}
 
-Player.prototype.updateScore = function() {
-  this.score++;
-}
+  didCollide(obstacle) {
+    const playerTop = this.y;
+    const playerBottom = this.y + this.size;
+    const playerLeft = this.x;
+    const playerRight = this.x + this.size;
 
-Player.prototype.receiveDamage = function(recivedDamage) {
-  this.damage += recivedDamage;
-}
+    const obstacleTop = obstacle.y;
+    const obstacleBottom = obstacle.y + obstacle.size;
+    const obstacleLeft = obstacle.x;
+    const obstacleRight = obstacle.x + obstacle.size;
 
-Player.prototype.repairDamage = function(removeDamage) {
-  if (this.damage >= 10) {
-    this.damage -= removeDamage;
+    const crossLeft = obstacleLeft <= playerRight && obstacleLeft >= playerLeft;
+    const crossRight = obstacleRight >= playerLeft && obstacleRight <= playerRight;
+    const crossBottom = obstacleBottom >= playerTop && obstacleBottom <= playerBottom;
+    const crossTop = obstacleTop <= playerBottom && obstacleTop >= playerTop;
+
+    if ((crossLeft || crossRight) && (crossTop || crossBottom)) {
+      return true;
+    }
+    return false;
   }
-}
 
-Player.prototype.removeLife = function(life) {
-  this.life -= life;
-}
+  updateScore() {
+    this.score++;
+  }
 
-Player.prototype.gainLife = function(life) {
-  if (this.life < 5) {
-    this.life += life;
+  receiveDamage(recivedDamage) {
+    this.damage += recivedDamage;
+  }
+
+  repairDamage(removeDamage) {
+    if (this.damage >= 10) {
+      this.damage -= removeDamage;
+    }
+  }
+
+  removeLife(life) {
+    this.life -= life;
+  }
+
+  gainLife(life) {
+    if (this.life < 5) {
+      this.life += life;
+    }
   }
 }
